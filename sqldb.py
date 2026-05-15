610c9aeb34c83bca5eb7776ffdd82cc9ae123d41
Hussein Rashid. <113936853+h0-qp@users.noreply.github.com>
2026-05-15 15:34:52 +0300

    Add files via upload

---
+++ sqldb.py
@@ -0,0 +1,15 @@
+from kvsqlite.sync import Client as C
+from config import database_name
+#انشاء
+#dev = [1160471152,435009958]
+db = C(f"audiobot.sqlite")
+db.autocommit = True
+#print(i)
+#members = []
+#db.delete(f"users")
+#for i in open("mems.txt","r").readlines():
+#	members.append(int(i.replace("\n","")))
+#	db.set(f"users",members)
+#	print(len(members))
+#print(len(db.get(f"users")))
+#print(members)
